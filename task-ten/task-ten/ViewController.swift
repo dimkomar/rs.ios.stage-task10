84fa77d9845e709bf8e256dafb5d7e4c9fa41a4c
Dmitriy Komar <dmitriyko@mbpw-dmitriyko.home>
2021-09-11 16:34:25 +0200

    init: create project

---
+++ task-ten/task-ten/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task-ten
+//
+//  Created by Dmitriy Komar on 11/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
